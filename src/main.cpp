7b5ddcecb61e45672066c19ecdb600f5faa8e777
SizzlinSeal <67880368+SizzlinSeal@users.noreply.github.com>
2020-12-03 08:39:57 -0800

    potentially fix motors randomly having a delay before stopping

--- src/main.cpp
+++ src/main.cpp
@@ -26,48 +26,56 @@
 void whenControllerR1Pressed() {
   intakeMove(600, rpm, forward);
   waitUntil(!Controller1.ButtonR1.pressing());
+  wait(5, msec);
   intakeStop();
 }
 
 void whenControllerR2Pressed() {
   intakeMove(400, rpm, reverse);
   waitUntil(!Controller1.ButtonR2.pressing());
+  wait(5, msec);
   intakeStop();
 }
 
 void whenControllerL1Pressed() {
   outakeMove(600, rpm, reverse);
   waitUntil(!Controller1.ButtonL1.pressing());
+  wait(5, msec);
   outakeStop();
 }
 
 void whenControllerL2Pressed() {
   outakeMove(400, rpm, forward);
   waitUntil(!Controller1.ButtonL2.pressing());
+  wait(5, msec);
   outakeStop();
 }
 
 void whenControllerButtonUpPressed() {
   driveTrainControl(forward, 15, percent);
   waitUntil(!Controller1.ButtonUp.pressing());
+  wait(5, msec);
   Drivetrain.stop();
 }
 
 void whenControllerButtonRightPressed() {
   driveTrainControl(right, 15, percent);
   waitUntil(!Controller1.ButtonRight.pressing());
+  wait(5, msec);
   Drivetrain.stop();
 }
 
 void whenControllerButtonDownPressed() {
   driveTrainControl(reverse, 15, percent);
   waitUntil(!Controller1.ButtonDown.pressing());
+  wait(5, msec);
   Drivetrain.stop();
 }
 
 void whenControllerButtonLeftPressed() {
   driveTrainControl(left, 15, percent);
   waitUntil(!Controller1.ButtonLeft.pressing());
+  wait(5, msec);
   Drivetrain.stop();
 }
 
