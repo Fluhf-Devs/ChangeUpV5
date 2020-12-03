2c329b5a807dcdb5622208dcbc599f1acff10f13
SizzlinSeal <67880368+SizzlinSeal@users.noreply.github.com>
2020-12-03 13:54:59 -0800

    revert previous commit

--- src/auto/mainAuto.cpp
+++ src/auto/mainAuto.cpp
@@ -17,22 +17,45 @@ void autoMain() {
   intakeMove(600, rpm, forward);
   wait(500, msec);
   intakeStop();
+  wait(1000, msec);
+  // go backwards
   driveTrainControl(reverse, 30, percent);
   wait(500, msec);
   Drivetrain.stop();
+  wait(1000, msec);
+  // turn 180 degrees
   Drivetrain.turnFor(left, 180, degrees);
+  wait(1000, msec);
+  // get the ball out
   intakeMove(600, rpm, reverse);
+  wait(1000, msec);
+  // move close to center goal
   Drivetrain.driveFor(forward, 455, mm);
+  wait(1000, msec);
+  // turn 90 degrees to the right 
   Drivetrain.turnFor(right, 90, degrees);
+  wait(1000, msec);
+  // go forward a bit so it can descore the middle balls
   Drivetrain.driveFor(forward, 50, mm);
+  wait(1000, msec);
+  // turn so it faces the center goal
   Drivetrain.turnFor(left, 90, degrees);
+  wait(1000, msec);
+  // ram to descore a ball at the center goal
   Drivetrain.drive(forward, 200, rpm);
   wait(500, msec);
   Drivetrain.stop();
-  Drivetrain.drive(reverse);
-  wait(350, msec);
-  Drivetrain.stop();
+  wait(1000, msec);
+  // score the middle with the preload
   outakeMove(600, rpm, reverse);
   wait(2000, msec);
   outakeStop();
+  wait(1000, msec);
+  // back out of the goal
+  Drivetrain.driveFor(reverse, 300, mm);
+  Drivetrain.stop();
+  wait(1000, msec);
+  // turn right
+  Drivetrain.turnFor(right, 90, degrees);
+  Drivetrain.stop();
 }
