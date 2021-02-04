33df91a6955eddfa935fc576dfadc6a04bceb144
SizzlinSeal <eduard.teale@gmail.com>
2021-02-04 13:26:41 -0800

    start work

---
+++ src/screenItems/controllerScreen.cpp
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       brainScreen.cpp                                           */
+/*    Author:       Liam Teale                                                */
+/*    Created:      Jan 31 2021                                               */
+/*    Description:  V5 ChangeUp Robot                                         */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+#include "functions.h"
+
+int controllerScreen() {
+  Controller1.Screen.clearScreen();
+  Controller1.Screen.setCursor(int32_t row, int32_t col);
+  Controller1.Screen.column();
+  Controller1.Screen.setCursor(int32_t row, int32_t col);
+  Controller1.Screen.column();
+
+  for(;;) {
+    Controller1
+  }
+
+
+
+  return 0;
+}
